45bc51a8bf8c574073d088b0b32aaf697808b7b7
slam24 <luismartinez242011@gmail.com>
2016-10-17 22:04:02 -0600

    feat: heading and objects response

--- helloHTML.js
+++ helloHTML.js
@@ -1,10 +1,12 @@
 var http = require('http'),
-	fs   = require('fs');
+    fs   = require('fs');
 
 // var html = fs.readFileSync('./index.html');
 var html = fs.readFile('./index.html', function(err,html){
-	http.createServer(function(req, res) {
-		res.write(html);
-		res.end();
-	}).listen(3000);	
+    http.createServer(function(req, res) {
+        // res.writeHead(200,{"Content-Type":"text/html"});
+        res.writeHead(200,{"Content-Type":"application/json"});
+        res.write(JSON.stringify({name:"luis",username:"slam24"}));
+        res.end();
+    }).listen(3000);
 });
