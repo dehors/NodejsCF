02c5fb0904c6acf7ce4ff0b7ef44c82f1c49f0c0
slam24 <luismartinez242011@gmail.com>
2016-11-10 21:15:51 -0600

    feat: Layout

--- Hello_express/app.js
+++ Hello_express/app.js
@@ -5,16 +5,24 @@ var app = express();
 app.set('view engine', 'jade');
 app.set('views', './views');
 
+// app.get("/",function(req,res){
+//   res.render("index",{title: 'My first page with express',message:'Hello world'});
+// });
+
+// app.get("/:var",function(req,res){
+//   res.render("form",{name:req.params.var});
+// });
+
+// app.post("/",function(req,res) {
+//   res.render("form");
+// });
+
 app.get("/",function(req,res){
-  res.render("index",{title: 'My first page with express',message:'Hello world'});
+  res.render("index");
 });
 
-app.get("/:var",function(req,res){
-  res.render("form",{name:req.params.var});
-});
-
-app.post("/",function(req,res) {
-  res.render("form");
+app.get("/login",function(req,res){
+  res.render("Partial/login");
 });
 
 app.listen(3000);
