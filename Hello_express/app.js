fa358f032b883150eac8f24a8edbe2acd565448a
slam24 <luismartinez242011@gmail.com>
2016-12-02 05:53:54 -0600

    feat: Virtual

--- Hello_express/app.js
+++ Hello_express/app.js
@@ -38,7 +38,7 @@ User.find(function(err,doc){
 });
 
 app.post("/users",function(req,res){
-  var user = new User({email:req.body.email,password:req.body.password});
+  var user = new User({email:req.body.email,password:req.body.password,password_confirmation: req.body.password_confirmation});
   user.save(function(){
     res.send('Usuario creado');
   });
