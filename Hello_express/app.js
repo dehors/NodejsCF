36c6090d1ca03efc4f12f200aae25d885ff7218e
slam24 <luismartinez242011@gmail.com>
2016-12-11 11:42:23 -0600

    feat: Custom validation -1

--- Hello_express/app.js
+++ Hello_express/app.js
@@ -38,7 +38,7 @@ User.find(function(err,doc){
 });
 
 app.post("/users",function(req,res){
-  var user = new User({email:req.body.email,password:req.body.password,password_confirmation: req.body.password_confirmation});
+  var user = new User({email:req.body.email,password:req.body.password,password_confirmation: req.body.password_confirmation,username:req.body.username});
   user.save(function(err){
     if(err){
       console.log(String(err));
--- Hello_express/model/user.js
+++ Hello_express/model/user.js
@@ -6,7 +6,16 @@ mongoose.connect('mongodb://localhost/SocialPicture');
 var user_schema = new Schema({
   name: String,
   username: {type:String,required:true,maxlength:[50,"The username must 50"]},
-  password: {type:String,minlength:[8,"The username must 0"]},
+  password: {
+    type:String,
+    minlength:[8,"The email must 0"],
+    validate:{
+      validator: function(p) {
+        return this.password_confirmation == p;
+      },
+      message: "Las contraseñas no son iguales"
+    }
+  },
   age: {type: Number,min:[5,"The age min 5"],max:[50,"The age must 50"]},
   email: {type: String,required: "The Email is required",match:[/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,"message"]},
   date_of_birth: Date,
