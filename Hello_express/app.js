0e401c492ec98dcd781df5f9aed7e28d74355385
slam24 <luismartinez242011@gmail.com>
2016-12-11 11:29:27 -0600

    feat: Validation

--- Hello_express/app.js
+++ Hello_express/app.js
@@ -39,8 +39,13 @@ User.find(function(err,doc){
 
 app.post("/users",function(req,res){
   var user = new User({email:req.body.email,password:req.body.password,password_confirmation: req.body.password_confirmation});
-  user.save(function(){
-    res.send('Usuario creado');
+  user.save(function(err){
+    if(err){
+      console.log(String(err));
+      res.send('Errorer');
+    }else{
+      res.send('Usuario creado'); 
+    }
   });
 });
 
--- Hello_express/model/user.js
+++ Hello_express/model/user.js
@@ -5,11 +5,12 @@ mongoose.connect('mongodb://localhost/SocialPicture');
 
 var user_schema = new Schema({
   name: String,
-  username: String,
-  password: String,
-  age: String,
-  email: String,
-  date_of_birth: Date
+  username: {type:String,required:true,maxlength:[50,"The username must 50"]},
+  password: {type:String,minlength:[8,"The username must 0"]},
+  age: {type: Number,min:[5,"The age min 5"],max:[50,"The age must 50"]},
+  email: {type: String,required: "The Email is required",match:[/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,"message"]},
+  date_of_birth: Date,
+  gender: {type: String,enum:{values:['M','F'],message:"message"}}
 });
 
 user_schema.virtual("password_confirmation").get(function(){
