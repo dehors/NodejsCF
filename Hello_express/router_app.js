bcfea84d45e5f1ff0b78cc93874d95b3acc82a32
slam24 <luismartinez242011@gmail.com>
2016-12-21 17:02:59 -0600

    feat: Router CRUD

--- Hello_express/router_app.js
+++ Hello_express/router_app.js
@@ -6,4 +6,34 @@ router.get("/", function(req,res) {
     res.render("app/home");
 });
 
+
+/* REST */
+
+router.get("/images/new", function(req,res) {
+    res.render("app/images/new");
+});
+
+router.get("/images/:id/edit", function(req,res) {
+    
+});
+
+router.route("images")
+    .get(function(req,res) {
+        
+    })
+    .post(function(req,res) {
+        
+    });
+
+router.route("images/:id")
+    .get(function(req,res) {
+        
+    })
+    .put(function(req,res) {
+        
+    })
+    .delete(function(req,res) {
+        
+    });
+
 module.exports = router;
