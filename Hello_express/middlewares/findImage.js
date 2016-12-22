36db5f02d531adc232e0dda44546a52d05ea2320
slam24 <luismartinez242011@gmail.com>
2016-12-22 10:23:36 -0600

    feat: Middleware permission

--- Hello_express/middlewares/findImage.js
+++ Hello_express/middlewares/findImage.js
@@ -1,10 +1,11 @@
 var Image = require('../model/image.js').Image;
+var owner_check = require("./image_permission");
 
 module.exports = function(req,res,next) {
   Image.findById(req.params.id)
     .populate("creator")
     .exec(function(err, imagen){
-        if(!err){
+        if(imagen != null && owner_check(imagen,req,res)){
             res.locals.imagen = imagen;
             next();
         }else{
